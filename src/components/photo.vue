f686ed2dc0c023727a47512011281ac8696be0fa
markal307 <markal307@users.noreply.github.com>
2019-12-12 21:49:14 +0100

    complete photogallery

--- src/components/photo.vue
+++ src/components/photo.vue
@@ -1,21 +1,46 @@
 <template>
     <div class="body">
+        <div class="text-center">
+            <v-btn
+                    color="error"
+                    @click="overlay = !overlay"
+            >
+                Show Overlay
+            </v-btn>
+
+            <v-overlay :value="overlay">
+                <v-img
+                        :src="'http://kaller.test'+overlayProp.path"
+                        height="90vh"
+                        contain
+                        @click="overlay = false"
+                        class="px-3 pt-3"
+                >
+                </v-img>
+                <v-btn
+                        icon
+                        @click="overlay = false"
+                >
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-overlay>
+        </div>
         <v-container>
 
             <v-row no-gutters>
                 <v-col v-for="photo in photos" :class="photo.col+' phototest'">
-                    <v-card class="px-2 pt-2">
+
+                    <v-card class="px-2 pt-2" max-height="500" @click="showOverlay(photo)">
                         <v-img
-                                contain
                                 :src="'http://kaller.test'+photo.path"
                         >
                         </v-img>
 
-                        <v-card-subtitle class="pb-0">{{photo.filename}}</v-card-subtitle>
+                        <v-card-subtitle class="pb-0">{{photo.lens}}</v-card-subtitle>
 
                         <v-card-text class="text--primary">
-                            <div>{{photo.aperture}}</div>
-                            <div>{{photo.shutterspeed}}s</div>
+                            <div>{{photo.aperture}} and {{photo.shutterspeed}}s</div>
+                            <div>{{photo.date_string}}</div>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -32,7 +57,13 @@
         data() {
             return {
                 photos: null,
-                height: 500
+                height: 500,
+                overlay: false,
+                overlayProp: {
+                    path: 'path',
+                    width: '',
+                    height: ''
+                },
             }
         },
         methods: {
@@ -40,9 +71,14 @@
                 let photos = await api.loadPhotos({limit: 100 });
                 console.log(photos);
                 let photo = photos[0];
-                console.log(photo.width/(photo.height/400));
                 this.photos = photos;
             },
+            showOverlay(photo){
+                this.overlayProp.path=photo.path;
+                this.overlayProp.width=photo.width;
+                this.overlayProp.height=photo.height;
+                this.overlay=true;
+            }
         },
         created() {
             this.requestPhotos()
@@ -53,6 +89,10 @@
 <style scoped>
     .phototest{
         padding: 8px;
+        height: 515px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     .body{
